4977f9648d6d0fc36a47d10927253d68443e0bf9
ortheog17@gmail.com <ortheog17@gmail.com>
2022-05-17 22:05:35 +0300

    initial commit

---
+++ ApiHW2/Controllers/FlowersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiHW2.Controllers
+{
+    [Route("api/flowers")]
+    [ApiController]
+    public class FlowersController : ControllerBase
+    {
+        [Route("Sun")]
+        public string SunFlower()
+        {
+            return "SunFlower";
+        }
+        [Route("narkis")]
+
+        public string Narkis()
+        {
+            return "Narkis";
+        }
+        [Route("rose")]
+
+        public string Rose()
+        {
+            return "Rose";
+        }
+        [Route("violete")]
+
+        public string Violete()
+        {
+            return "Violete";
+        }
+    }
+}
